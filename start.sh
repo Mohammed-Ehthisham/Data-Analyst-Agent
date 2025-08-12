e4038ce0210af0bd513420abdf6cfa2eecc70574
Mohammed Ehthisham Ullah Khan <mohkhan@lab3r6s9.42abudhabi.ae>
2025-08-12 15:09:14 +0400

    cleaned the deployemnt

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Render start script
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Start the FastAPI application with Gunicorn
-gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
